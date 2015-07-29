07045747e96a5978b6dd7ef385dde6792307af39
Edil Ashimov <edilashimov@Edils-MacBook-Pro.local>
2015-07-29 10:50:29 -0500

    Initial Commit

---
+++ Go To Jail/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Go To Jail
+//
+//  Created by Edil Ashimov on 7/29/15.
+//  Copyright (c) 2015 Edil Ashimov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Go To Jail/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Go To Jail
+//
+//  Created by Edil Ashimov on 7/29/15.
+//  Copyright (c) 2015 Edil Ashimov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
